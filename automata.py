64f4f17431290f909db4094654978a81189e76bc
teberger <tberge01@cs.unm.edu>
2014-09-18 17:32:15 -0600

    /home/taylor/git/cs554-project1/automata.py autocommit from emacs: Thu Sep 18 17:32:15 2014

---
+++ automata.py
@@ -0,0 +1,19 @@
+class Automota:
+    def __init__(self, type, states, start_state, accept_states):
+        self.type = type
+        self.states = states
+        self.start_states = start_states
+        self.accept_states = accept_states
+
+
+class Node:
+    def __init__(self, node_name, transitions):
+        self.node_name = node_name
+        self.transitions = transitions
+
+    def transition(to):
+        if transitions[to] == null:
+            return null;
+        else:
+            return transitions[to]
+    
