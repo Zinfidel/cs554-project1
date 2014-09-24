6e52a2eaa005276ef3e0f37b7e7a14161c531738
Taylor <tberge01@cs.unm.edu>
2014-09-23 23:18:20 -0600

    /home/taylor/git/cs554-project1/scanner.py autocommit from emacs: Tue Sep 23 23:18:20 2014

--- scanner.py
+++ scanner.py
@@ -67,4 +67,4 @@ AutomataTokenDictionary = {y:x for (x,y) in enumerate(["States", "Start", "Accep
 
 if __name__ == "__main__":
 #    print OneOrMore(Word(alphas)).setDebug().parseString("asdfa asd dkjlasdf dsf")
-#    ConstructAutomata(open("testdata/dfa2.txt").read())
+    ConstructAutomata(open("testdata/dfa2.txt").read())
