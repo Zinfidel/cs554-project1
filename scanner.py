529aa369415fcc57955c3c2c4126db5b2c06a4cb
Taylor <tberge01@cs.unm.edu>
2014-09-23 23:21:12 -0600

    /home/taylor/git/cs554-project1/scanner.py autocommit from emacs: Tue Sep 23 23:21:12 2014

--- scanner.py
+++ scanner.py
@@ -31,7 +31,7 @@ AcceptingStates = accept_keyword + StateList + end_keyword
 
 transitions_keyword = Keyword("transitions").suppress()
 Transition = State + SymbolList + arrow + State
-TransitionList = ZeroOrMore(Transition)
+TransitionList = delimitedList(Transition, delim='\n')
 Transitions = transitions_keyword + TransitionList + end_keyword
 
 automata_keyword = Keyword("dfa").suppress() ^ Keyword("nfa").suppress()
