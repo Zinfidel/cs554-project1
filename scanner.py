87262804b7f27dea8055f00f76453ae916bac360
Taylor <tberge01@cs.unm.edu>
2014-09-23 23:07:16 -0600

    /home/taylor/git/cs554-project1/scanner.py autocommit from emacs: Tue Sep 23 23:07:16 2014

--- scanner.py
+++ scanner.py
@@ -15,7 +15,7 @@ alphabet_keyword = Keyword("alphabet").suppress()
 alphabet_end_keyword = Keyword("end").suppress()
 #Symbol = Word("\'", alphas, exact=2)
 Tick = Keyword("\'")
-SymbolList = Keyword("\'") + delimitedList(alphanums, delim="'")
+SymbolList = Word("\'") + delimitedList(alphanums, delim="'")
 #SymbolList = OneOrMore(Symbol) + Keyword('\n')
 #SymbolList = OneOrMore(Symbol)
 Alphabet = alphabet_keyword + SymbolList + alphabet_end_keyword
