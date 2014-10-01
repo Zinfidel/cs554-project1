135d64553cdb557435ac734f8a44ad9d6ef5703e
teberger <tberge01@cs.unm.edu>
2014-10-01 12:17:38 -0600

    /home/taylor/git/cs554-project1/scanner.py autocommit from emacs: Wed Oct  1 12:17:38 2014

--- scanner.py
+++ scanner.py
@@ -64,5 +64,5 @@ Regex = ZeroOrMore(Literal('*') | \
 if __name__ == "__main__":
     print Regex.parseString("* | 'a + 'a ' ")
     print Symbol.parseString('\' ') # <---- TODO: This should work... ' ' can
-                                    # be part of the alphabet
+                                    #             be part of the alphabet
 
