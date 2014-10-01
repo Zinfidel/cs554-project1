74dd606b69db3477c29d1f3b7c7688b5b3a6bfb7
teberger <tberge01@cs.unm.edu>
2014-09-30 21:21:32 -0600

    /home/taylor/git/cs554-project1/scanner.py autocommit from emacs: Tue Sep 30 21:21:32 2014

--- scanner.py
+++ scanner.py
@@ -57,4 +57,4 @@ def ConstructAutomata(file):
 if __name__ == "__main__":
     dfa = ConstructAutomata("testdata/dfa2.txt")
     print dfa.nodes
-    print dfa.nodes['s1'].getTransitions('a')
+    print dfa.nodes['s3'].getTransitions('a')
