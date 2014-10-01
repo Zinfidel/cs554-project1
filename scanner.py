784fbad27e50ee1f2343dd0fab61ba318a4d16f9
teberger <tberge01@cs.unm.edu>
2014-10-01 12:09:27 -0600

    /home/taylor/git/cs554-project1/scanner.py autocommit from emacs: Wed Oct  1 12:09:27 2014

--- scanner.py
+++ scanner.py
@@ -56,7 +56,7 @@ def ConstructAutomata(file):
 
 
 # Regex parsing:
-Regex = ZeroOrMore(Literal('*') | Literal('|') | Literal('+') | (Literal('\'') + Word(alphas + ' ')))
+Regex = ZeroOrMore(Literal('*') | Literal('|') | Literal('+') | (Literal('\'').suppress() + Word(alphas + ' ')))
 
 if __name__ == "__main__":
     print Regex.parseString("* | 'a + 'a ' ")
