f856edfcf4429474a8f693737e331a4eab6d86f9
teberger <tberge01@cs.unm.edu>
2014-10-01 12:07:59 -0600

    /home/taylor/git/cs554-project1/scanner.py autocommit from emacs: Wed Oct  1 12:07:59 2014

--- scanner.py
+++ scanner.py
@@ -59,5 +59,5 @@ def ConstructAutomata(file):
 Regex = ZeroOrMore(Literal('*') | Literal('|'))
 
 if __name__ == "__main__":
-    print Regex.matchString("* * *")
+    print Regex.parseString("* * *")
 
