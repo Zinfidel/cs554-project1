afbe4c12511847b55cd1049dc3bb83a68c1e000b
Zinfidel <yamkoo@gmail.com>
2014-10-02 01:26:00 -0600

    Implemented lexical desc parsing.
    Fixed the symbol whitespace thing again too. Next a class needs to be made
    for the lexical descriptions that will contain regex objects.

--- scanner.py
+++ scanner.py
@@ -1,5 +1,3 @@
-
-
 #!/usr/bin/env python
 
 """scanner.py: Contains scanner/lexer/tokenizer functions for reading from a file."""
@@ -8,19 +6,31 @@ from pyparsing import *
 from automata import Automata
 from regex import *
 
-# General definitions
+#######################
+# General definitions #
+#######################
 arrow = Keyword("-->").suppress()
 end_keyword = Keyword("end;").suppress()
 
 # Alphabet definition
 alphabet_keyword = Keyword("alphabet").suppress()
-alphabet_end_keyword = Keyword("end").suppress()
-Symbol = Literal("\'").suppress() + (White(exact=1) ^ Word(alphas, exact=1))
+alphabet_end_keyword = Keyword("end;").suppress() | Keyword("end").suppress()
+Symbol = Combine(Literal("\'") + Optional(Literal("\\")) + Word(alphas + " ", exact=1))
 SymbolList = OneOrMore(Symbol)
 Alphabet = alphabet_keyword + SymbolList + alphabet_end_keyword
 # example: ['a, 'b, 'c]
 
-# DFA/NFA definition
+# Regex definition:
+Regex = ZeroOrMore(Literal('*') ^
+                   Literal('|') ^
+                   Literal('+') ^
+                   Symbol)
+# example: ['b, +, *, |, 'a, 'o, 'r]
+
+
+######################
+# DFA/NFA definition #
+######################
 states_keyword = Keyword("states").suppress()
 State = ~end_keyword + Word(alphanums)
 StateList = ZeroOrMore(State)
@@ -42,12 +52,41 @@ Transitions = transitions_keyword + TransitionList + end_keyword
 # example: [ [s1, ['a, 'b], s2], [s2, ['a], s3] ]
 
 automata_keyword = Keyword("dfa").suppress() ^ Keyword("nfa").suppress()
-FiniteAutomata = automata_keyword\
-                 + Group(States).setResultsName("States")\
-                 + Group(InitialState).setResultsName("Start")\
-                 + Group(AcceptingStates).setResultsName("Accept")\
-                 + Group(Transitions).setResultsName("Transitions")\
+FiniteAutomata = automata_keyword \
+                 + Group(States).setResultsName("States") \
+                 + Group(InitialState).setResultsName("Start") \
+                 + Group(AcceptingStates).setResultsName("Accept") \
+                 + Group(Transitions).setResultsName("Transitions") \
                  + Group(Alphabet).setResultsName("Alphabet")
+
+
+###############################
+# Complete Lexical Definition #
+###############################
+identifier = Word(printables)
+
+relevant_keyword = Keyword("relevant")
+irrelevant_keyword = Keyword("irrelevant")
+discard_keyword = Keyword("discard")
+SemanticRelevance = relevant_keyword ^ irrelevant_keyword ^ discard_keyword
+
+ClassDescription = Group(Regex)
+# example: ["'b", "'a", "'o", "'r", "'i", "'n", "'g", "' ", "'\\n"]
+
+class_keyword = Keyword("class").suppress()
+is_keyword = Keyword("is").suppress()
+Class = class_keyword + identifier + is_keyword + ClassDescription + SemanticRelevance + end_keyword
+# example: ['base', ['+', "'b", '+', '*', '|', "'a", "'o", "'r"], 'relevant'],
+
+ClassList = ZeroOrMore(Group(Class))
+
+language_keyword = Keyword("language").suppress()
+LexicalDescription = language_keyword \
+                     + identifier.setResultsName("Name") \
+                     + Group(Alphabet).setResultsName("Alphabet") \
+                     + Group(ClassList).setResultsName("Classes") \
+                     + end_keyword
+# example: [z++, [ [class 1], [class 2], [class 3] ] ]
 
 
 def ConstructAutomata(file):
@@ -58,33 +97,30 @@ def ConstructAutomata(file):
     return Automata(fa.States, fa.Start, fa.Accept, fa.Transitions)
 
 
-# Regex parsing:
-Regex = ZeroOrMore(Literal('*') | \
-                   Literal('|') | \
-                   Literal('+') | \
-                   Symbol)
+
 
 def ConstructRegex(file):
-    '''Parses the supplied regex, and constructs the appropriate Regex data structure found in ./regex.py
+    """Parses the supplied regex, and constructs the appropriate Regex data structure found in ./regex.py
        
        The supplied input an be a file object or a URI.
-    '''
+    """
     regex_tokens = Regex.parseFile(file)
     r, leftover = BuildExpression(regex_tokens)
     pass
 
+
 def BuildExpression(tokens):
-    '''Builds an expression from a list of tokens using a one token look ahead
+    """Builds an expression from a list of tokens using a one token look ahead
        strategy. 
 
        tokens: Expected to be a list of string tokens (ie: ['+', 'a', 'a'])
-    '''
+    """
     t = tokens[0]
 
     # E -> + E E
     if t == '+':
-#TODO: Clean this up
-#        return BuildConcatenation(tokens[1:])a
+        # TODO: Clean this up
+        #        return BuildConcatenation(tokens[1:])a
         # build the appropriate expression for the left argument to the concat
         # operation and return the leftover tokens
         leftSide, leftover = BuildExpression(tokens[1:])
@@ -121,14 +157,14 @@ def BuildExpression(tokens):
         return Sigma(t), tokens[1:]
 
 
-
 if __name__ == "__main__":
-    tokens = Regex.parseString("+ 'a * 'a")
-    print tokens
-    e, l = BuildExpression(tokens)
-    print e
-    # print c.matches('aa')
-    print e.matches('aaaaaa')
-    print l
-    print Symbol.parseString('\' ') # <---- TODO: This should work... ' ' can
-                                    #             be part of the alphabet
+    # tokens = Regex.parseString("+ 'a * 'a")
+    # print tokens
+    # e, l = BuildExpression(tokens)
+    # print e
+    # # print c.matches('aa')
+    # print e.matches('aaaaaa')
+    # print l
+    # print Symbol.parseString('\' ')  # <---- TODO: This should work... ' ' can
+    # # be part of the alphabet
+    print LexicalDescription.parseFile("testdata/lexdesc1.txt")
