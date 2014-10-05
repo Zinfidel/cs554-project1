495d340588477fbdfadbfc2facb9771b0bf70bf0
Zinfidel <yamkoo@gmail.com>
2014-10-05 14:57:13 -0600

    Changed alphas to printables.
    Turns out symbols can be characters, numbers, punctuation, all sorts of stuff.

--- scanner.py
+++ scanner.py
@@ -15,7 +15,7 @@ end_keyword = Keyword("end;").suppress()
 # Alphabet definition
 alphabet_keyword = Keyword("alphabet").suppress()
 alphabet_end_keyword = Keyword("end;").suppress() | Keyword("end").suppress()
-Symbol = Combine(Literal("\'") + Optional(Literal("\\")) + Word(alphas + " ", exact=1))
+Symbol = Combine(Literal("\'") + Optional(Literal("\\")) + Word(printables + " ", exact=1))
 SymbolList = OneOrMore(Symbol)
 Alphabet = alphabet_keyword + SymbolList + alphabet_end_keyword
 # example: ['a, 'b, 'c]
