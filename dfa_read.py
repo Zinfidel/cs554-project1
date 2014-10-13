2568372d8a5fe128203b18f83d1a6f5d1f3efabd
Zinfidel <yamkoo@gmail.com>
2014-10-12 22:18:18 -0600

    Fixed dfa_read.
    Last bug was that the function getTransitionState returns a list of transition states (because of NFAs) but dfa_read was expecting a single string. Added a dereference to ensure this was the case.

--- dfa_read.py
+++ dfa_read.py
@@ -6,7 +6,7 @@ def dfa_valid_string(automata, testing_string, current_state, current_step):
             if current_state == x: return True
         return False
     else:
-        next_state = dfa.nodes[current_state].getTransitionState(testing_string[current_step])
+        next_state = dfa.nodes[current_state].getTransitionState(testing_string[current_step])[0] # Only one state!
         if next_state is None: return False
         else: return dfa_valid_string(automata, testing_string, next_state, current_step + 1)
 
