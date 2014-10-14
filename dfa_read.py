9517949219c260ea32537b0ce7cfb9dc36bb813d
teberger <tberge01@cs.unm.edu>
2014-10-14 16:02:00 -0600

    fixed margins in dfa_read

--- dfa_read.py
+++ dfa_read.py
@@ -6,6 +6,9 @@ def dfa_valid_string(automata, testing_string, current_state, current_step):
             if current_state == x: return True
         return False
     else:
-        next_state = dfa.nodes[current_state].getTransitionState(testing_string[current_step])[0] # Only one state!
+        # Since there is only one state!
+        next_state = dfa.nodes[current_state].getTransitionState(testing_string\
+                                                                 [current_step])[0] 
         if next_state is None: return False
-        else: return dfa_valid_string(automata, testing_string, next_state, current_step + 1)
+        else: return dfa_valid_string(automata, testing_string, \
+                                      next_state, current_step + 1)
